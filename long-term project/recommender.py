47e7748bcbdb25e82ea4ae74c875c02beae02108
pch6828 <pch6828@hanyang.ac.kr>
2021-05-16 00:41:20 +0900

    Recommender : project initialize

--- long-term project/recommender.py
+++ long-term project/recommender.py
@@ -1,8 +1,12 @@
 import sys
 import time
+import numpy as np
+import pandas as pd
 
 def main(argv):
-    pass
+    if len(argv)<3:
+        print('PLEASE, give 2 arguments (train filename, test filename)')
+        return
 
 if __name__ == '__main__':
     main(sys.argv)
