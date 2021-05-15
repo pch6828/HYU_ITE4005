7108d56bff62d8ed840eccf5819d9ae1ed7a6a3f
pch6828 <pch6828@hanyang.ac.kr>
2021-05-16 00:48:41 +0900

    DBSCAN : project initialize

---
+++ assignment3/clustering.py
@@ -0,0 +1,12 @@
+import sys
+import time
+import numpy as np
+import pandas as pd
+
+def main(argv):
+    if len(argv)<5:
+        print('PLEASE, give 4 arguments (input filename, N, EPS, MinPTS)')
+        return
+
+if __name__ == '__main__':
+    main(sys.argv)
